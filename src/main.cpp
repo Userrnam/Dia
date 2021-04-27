c7a84bc39278497cf634aa40c79bb5cd966ad9aa
Author <43812455+Userrnam@users.noreply.github.com>
2021-04-27 15:23:04 +0300

    add Delete mode

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 
 
 #define MAX_SELECT_DISTANCE 20
+#define MAX_DELETE_DISTANCE 20
+#define EPS 1e-4
 
 struct Line
 {
@@ -17,27 +19,66 @@ struct Line
 	}
 };
 
-enum class AppMode
+bool same(sf::Vector2f v1, sf::Vector2f v2)
 {
-	Edit,
-	Create
+	return fabs(v1.x - v2.x) < EPS && fabs(v1.y - v2.y) < EPS;
+}
+
+#define MODE_NUMBER(mode, number)\
+	number << 3 | mode
+
+#define CREATE_MODE 1
+#define EDIT_MODE   2
+#define DELETE_MODE 3
+
+enum class State
+{
+	CNone            = MODE_NUMBER(CREATE_MODE, 0),
+	CNewLine         = MODE_NUMBER(CREATE_MODE, 1),
+
+	ENone            = MODE_NUMBER(EDIT_MODE, 0),
+	EMovingLine      = MODE_NUMBER(EDIT_MODE, 1),
+	EMovingLinePoint = MODE_NUMBER(EDIT_MODE, 2),
+
+	DNone            = MODE_NUMBER(DELETE_MODE, 0),
 };
 
-std::string modeAsString(AppMode mode)
+int Mode(State state)
 {
-	if (mode == AppMode::Edit) return "Edit Mode";
-	if (mode == AppMode::Create) return "Create Mode";
+	return (int)state & 0b111;
+}
+
+std::string stateAsString(State state)
+{
+	switch (state)
+	{
+	case State::CNone:
+		return "Create Mode";
+	case State::CNewLine:
+		return "Create Line";
+	case State::ENone:
+		return "Edit Mode";
+	case State::EMovingLine:
+		return "Edit Line";
+	case State::EMovingLinePoint:
+		return "Edit Line Point";
+	case State::DNone:
+		return "Delete Mode";
+	default: {}
+	}
 	return "Error";
 }
 
 struct AppInfo
 {
-	AppMode mode = AppMode::Create;
+	State  state = State::CNone;
 	int characterSize = 30;
 	sf::Font font;
 	sf::RenderWindow *window;
 	sf::Vector2i windowSize;
 	int gridSize = 50;
+
+	sf::Vector2f *pVec;
 
 	std::vector<Line> lines;
 };
@@ -139,21 +180,12 @@ void drawAppMode(AppInfo *info)
 	text.setPosition(10, 10);
 	text.setFillColor(sf::Color::Black);
 
-	text.setString(modeAsString(info->mode));
+	text.setString(stateAsString(info->state));
 
 	info->window->draw(text);
 }
 
 const auto defaultWindowSize = sf::Vector2i(800, 600);
-
-enum class State
-{
-	None,
-	CreatingNewLine,
-	Editing,
-	MovingLine,
-	MovingLinePoint
-};
 
 sf::Vector2f snap(AppInfo *info, sf::Vector2f pos)
 {
@@ -169,10 +201,11 @@ float d2(sf::Vector2f v1, sf::Vector2f v2)
 	return v.x * v.x + v.y * v.y;
 }
 
-sf::Vector2f *getClosestPoint(AppInfo *info, sf::Vector2f pos, float *distance2)
+sf::Vector2f *getClosestPoint(AppInfo *info, sf::Vector2f pos, float *distance2, Line **pline = nullptr)
 {
-	sf::Vector2f *res = &info->lines[0].p[0];
-	float mind = 1e5;
+	sf::Vector2f *res = nullptr;
+	Line *l = nullptr;
+	float mind = 1e15;
 	for (auto &line : info->lines)
 	{
 		for (int i = 0; i < 2; ++i)
@@ -182,11 +215,200 @@ sf::Vector2f *getClosestPoint(AppInfo *info, sf::Vector2f pos, float *distance2)
 			{
 				mind = d;
 				res = &line.p[i];
+				l = &line;
 			}
 		}
 	}
 	*distance2 = mind;
+	if (pline)
+	{
+		*pline = l;
+	}
 	return res;
+}
+
+float d2line(Line& l, sf::Vector2f pos)
+{
+	float a2 = d2(l.p[0], pos);
+	float b2 = d2(l.p[1], pos);
+	float c2 = d2(l.p[0], l.p[1]);
+
+	float c = sqrt(c2);
+
+	float t = (a2 - b2 - c2)/(2*c);
+
+	return b2 - t*t;
+}
+
+Line *getClosestLine(AppInfo *info, sf::Vector2f pos, float *distance2)
+{
+	float d;
+	Line *l;
+	getClosestPoint(info, pos, &d, &l);
+
+	for (auto& line : info->lines)
+	{
+		float dd = d2line(line, pos);
+		if (dd < d)
+		{
+			d = dd;
+			l = &line;
+		}
+	}
+
+	*distance2 = d;
+
+	return l;
+}
+
+void handleEditMode(AppInfo *info, sf::Event& e)
+{
+	switch (e.type)
+	{
+		case sf::Event::MouseMoved:
+		{
+			sf::Vector2f mousePos = sf::Vector2f(e.mouseMove.x, e.mouseMove.y);
+			if (info->state == State::EMovingLinePoint)
+			{
+				*info->pVec = snap(info, mousePos);
+			}
+
+			break;
+		}
+
+		case sf::Event::MouseButtonPressed:
+		{
+			if (info->state == State::ENone)
+			{
+				float dist2;
+				sf::Vector2f *p = getClosestPoint(info, sf::Vector2f(e.mouseButton.x, e.mouseButton.y), &dist2);
+				if (dist2 < MAX_SELECT_DISTANCE * MAX_SELECT_DISTANCE)
+				{
+					info->state = State::EMovingLinePoint;
+					info->pVec = p;
+				}
+			}
+
+			break;
+		}
+
+		case sf::Event::MouseButtonReleased:
+		{
+			if (info->state == State::EMovingLinePoint)
+			{
+				info->state = State::ENone;
+				info->pVec = nullptr;
+				// FIXME: 2 points of the same line may be the same.
+				// If this happens, the line should be deleted
+			}
+
+			break;
+		}
+
+		// TODO move this from here
+		case sf::Event::KeyPressed:
+		{
+			if (e.key.code == sf::Keyboard::C)
+			{
+				info->state = State::CNone;
+			}
+			if (e.key.code == sf::Keyboard::D)
+			{
+				info->state = State::DNone;
+			}
+			break;
+		}
+
+		default: {}
+	}
+}
+
+void handleCreateMode(AppInfo *info, sf::Event& e)
+{
+	sf::Vector2f *pVec = nullptr;
+	switch (e.type)
+	{
+		case sf::Event::MouseMoved:
+		{
+			sf::Vector2f mousePos = sf::Vector2f(e.mouseMove.x, e.mouseMove.y);
+			if (info->state == State::CNewLine)
+			{
+				info->lines.back().p[1] = snap(info, mousePos);
+			}
+			break;
+		}
+
+		case sf::Event::MouseButtonPressed:
+		{
+			info->state = State::CNewLine;
+
+			info->lines.push_back({});
+			info->lines.back().p[0] = snap(info, sf::Vector2f(e.mouseButton.x, e.mouseButton.y));
+			info->lines.back().p[1] = info->lines.back().p[0];
+
+			break;
+		}
+
+		case sf::Event::MouseButtonReleased:
+		{
+			if (info->state == State::CNewLine)
+			{
+				info->lines.back().p[1] = snap(info, sf::Vector2f(e.mouseButton.x, e.mouseButton.y));
+				info->state = State::CNone;
+				if (same(info->lines.back().p[0], info->lines.back().p[1]))
+				{
+					info->lines.pop_back();
+				}
+			}
+
+			break;
+		}
+
+		case sf::Event::KeyPressed:
+		{
+			if (e.key.code == sf::Keyboard::E)
+			{
+				info->state = State::ENone;
+			}
+			else if (e.key.code == sf::Keyboard::D)
+			{
+				info->state = State::DNone;
+			}
+			break;
+		}
+
+		default: {}
+	}
+}
+
+void handleDeleteMode(AppInfo *info, sf::Event& e)
+{
+	if (e.type == sf::Event::MouseButtonPressed)
+	{
+		sf::Vector2f mousePos = sf::Vector2f(e.mouseButton.x, e.mouseButton.y);
+		float dist2;
+		Line *l = getClosestLine(info, mousePos, &dist2);
+
+		// remove line
+		if (dist2 < MAX_DELETE_DISTANCE * MAX_DELETE_DISTANCE)
+		{
+			int index = l - info->lines.data();
+			
+			info->lines.erase(info->lines.begin() + index);
+		}
+	}
+
+	if (e.type == sf::Event::KeyPressed)
+	{
+		if (e.key.code == sf::Keyboard::E)
+		{
+			info->state = State::ENone;
+		}
+		else if (e.key.code == sf::Keyboard::C)
+		{
+			info->state = State::CNone;
+		}
+	}
 }
 
 int main()
@@ -201,107 +423,38 @@ int main()
 
 	app.font.loadFromFile("resources/Hack-Regular.ttf");
 
-	State state;
-	state = State::None;
-	sf::Vector2f *pVec = nullptr;
-
 	while (window.isOpen())
 	{
 		sf::Event e;
 		if (window.waitEvent(e))
 		{
-			switch (e.type)
+			if (e.type == sf::Event::Closed)  window.close();
+			if (e.type == sf::Event::Resized)
 			{
-				case sf::Event::Closed:
-				{
-					window.close();
-					break;
-				}
-
-				case sf::Event::Resized:
-				{
-					sf::FloatRect visibleArea(0, 0, e.size.width, e.size.height);
-					window.setView(sf::View(visibleArea));
-					app.windowSize = sf::Vector2i(e.size.width, e.size.height);
-					break;
-				}
-
-				case sf::Event::MouseMoved:
-				{
-					sf::Vector2f mousePos = sf::Vector2f(e.mouseMove.x, e.mouseMove.y);
-					if (state == State::CreatingNewLine)
-					{
-						app.lines.back().p[1] = snap(&app, mousePos);
-					}
-					else if (state == State::MovingLinePoint)
-					{
-						*pVec = snap(&app, mousePos);
-					}
-
-					break;
-				}
-
-				case sf::Event::MouseButtonPressed:
-				{
-					if (state == State::None)
-					{
-						state = State::CreatingNewLine;
-
-						app.lines.push_back({});
-						app.lines.back().p[0] = snap(&app, sf::Vector2f(e.mouseButton.x, e.mouseButton.y));
-						app.lines.back().p[1] = app.lines.back().p[0];
-					}
-					else if (state == State::Editing)
-					{
-						float dist2;
-						sf::Vector2f *p = getClosestPoint(&app, sf::Vector2f(e.mouseButton.x, e.mouseButton.y), &dist2);
-						if (dist2 < MAX_SELECT_DISTANCE * MAX_SELECT_DISTANCE)
-						{
-							state = State::MovingLinePoint;
-							pVec = p;
-						}
-					}
-
-					break;
-				}
-
-				case sf::Event::MouseButtonReleased:
-				{
-					if (state == State::CreatingNewLine)
-					{
-						app.lines.back().p[1] = snap(&app, sf::Vector2f(e.mouseButton.x, e.mouseButton.y));
-						state = State::None;
-						if (app.lines.back().p[0] == app.lines.back().p[1])
-						{
-							app.lines.pop_back();
-						}
-					}
-					else if (state == State::MovingLinePoint)
-					{
-						state = State::Editing;
-						pVec = nullptr;
-					}
-
-					break;
-				}
-
-				case sf::Event::KeyPressed:
-				{
-					if (e.key.code == sf::Keyboard::E)
-					{
-						app.mode = AppMode::Edit;
-						state = State::Editing;
-					}
-					else if (e.key.code == sf::Keyboard::C)
-					{
-						app.mode = AppMode::Create;
-						state = State::None;
-					}
-					break;
-				}
-
-				default: {}
+				sf::FloatRect visibleArea(0, 0, e.size.width, e.size.height);
+				window.setView(sf::View(visibleArea));
+				app.windowSize = sf::Vector2i(e.size.width, e.size.height);
 			}
+
+			switch (Mode(app.state))
+			{
+				case CREATE_MODE:
+				{
+					handleCreateMode(&app, e);
+					break;
+				}
+				case EDIT_MODE:
+				{
+					handleEditMode(&app, e);
+					break;
+				}
+				case DELETE_MODE:
+				{
+					handleDeleteMode(&app, e);
+					break;
+				}
+			}
+
 		}
 
 		window.clear(sf::Color::White);
